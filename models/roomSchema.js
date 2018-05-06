1666eeec6d374adbeef7c64f3fa384bb9da62ae8
Kim Minho <minmin0416@naver.com>
2018-05-06 15:23:31 +0900

    Delete models. No need mongodb anymore socket io 사용으로 인해 몽고디비 사용 안함

--- models/roomSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoClient = require('mongoose');
-
-const roomSchema = new mongoClient.Schema({
-    roomId: String,
-    Users: Array,
-});
-
-module.exports = mongoClient.model('room', roomSchema);
--- models/userSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoClient = require('mongoose');
-
-const userSchema = new mongoClient.Schema({
-    uid: String,
-    ip: String,
-    port:String
-});
-
-module.exports = mongoClient.model('user', userSchema);

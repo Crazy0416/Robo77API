5fa3bf7fbfe606cf694d86b5c63665a1910c2d54
Kim Minho <minmin0416@naver.com>
2018-05-06 17:06:04 +0900

    Add redis module 레디스 모듈 추가

--- modules/socketioHandler.js
+++ modules/socketioHandler.js
@@ -1,6 +1,27 @@
+const redis = require('../modules/redisHandler');
+
 exports = module.exports = function(io) {
     io.on('connection', (socket) => {
-        console.log(socket.address, socket.ip, " connected");
+        console.log(socket.id ," connected");
+
+        socket.on('joinRoom', function(msg) {
+            console.log('joinRoom name', msg);
+            socket.join(msg, () => {
+                console.log(socket.id, ' client\' in room ', Object.keys(socket.rooms)[0]);
+            });
+        });
+
+        socket.on('createRoom', function(msg) {
+            console.log('joinRoom name', msg);
+            redis.set('room'+msg, socket.id, function(err) {
+               if(err) console.log(err);
+               console.log(socket.id, " set dealer at redis");
+            });
+
+            socket.join(msg, () => {
+                console.log(socket.id, ' client\' in room ', Object.keys(socket.rooms)[0]);
+            });
+        });
 
         socket.on('chat message', function(msg){
             io.emit('chat message', msg);
@@ -8,6 +29,7 @@ exports = module.exports = function(io) {
     });
 
     io.on('disconnect', () => {
+        // Rooms are left automatically upon disconnection
         console.log('disconnected');
     });
 };
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,20 +1,49 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const io = require('socket.io');
 
 router.get('/', function(req, res, next) {
-    console.log(path.join(__dirname, "../public/socketTest.html"));
     res.sendFile(path.join(__dirname, "../public/socketTest.html"));
 });
 
-/* POST 딜러가 방 생성*/
+/* POST 딜러가 방 생성
+*  일단 room1 만 만듦
+* */
 router.post('/', function(req, res, next) {
+    let io = req.app.get('socketio');
 
+    // TODO: room 1이 존재하는 지 확인 -> room이 여러개 생기면 나중에 바꿔야함
+    if(io.sockets.adapter.rooms['1']) {
+        console.log("1 room already exist");
+        res.status(403).json({
+            "success": false,
+            "message": "1 room already exist"
+        })
+    } else {
+        res.json({
+            "success": true,
+            "roomId" : "1",
+            "message": "1 room create"
+        })
+    }
 });
 
 /* GET 방 참가 */
 router.get('/:roomId', function(req, res, next) {
+    let io = req.app.get('socketio');
 
+    if(io.sockets.adapter.rooms['1']) {
+        console.log("1 room already exist");
+        res.status(403).json({
+            "success": false
+        })
+    } else {
+        res.json({
+            "success": true,
+            "roomId" : "1"
+        })
+    }
 });
 
 

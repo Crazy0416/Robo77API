0c3827e1209ee9029481d124f66d557500c25794
Kim Minho <minmin0416@naver.com>
2018-05-06 18:05:45 +0900

    Add redis Handler redis의 handler를 만듦

--- modules/redisHandler.js
+++ modules/redisHandler.js
@@ -1,4 +1,12 @@
 const redis = require('redis');
 const redisClient = redis.createClient();
 
+redisClient.on('connect', () => {
+   console.log("Connected to Redis");
+});
+
+redisClient.on('error', function(err) {
+    console.log('Redis error: ' + err);
+});
+
 module.exports = redisClient;

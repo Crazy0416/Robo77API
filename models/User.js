93608ceb3afa5c590f00e7c098a02adbd571eca6
Kim Minho <minmin0416@naver.com>
2018-05-09 13:16:14 +0900

    Add User.heart. 하트(게임코인) 프로퍼티 추가

--- models/User.js
+++ models/User.js
@@ -1,9 +1,11 @@
 const CardList = require('../modules/CardList');
+const HEART_CNT = 5;
 
 class User {
     constructor(socketId) {
         this.socketId = socketId;
         this.cardList = new CardList();
+        this.heart = HEART_CNT;
     }
 }
 
